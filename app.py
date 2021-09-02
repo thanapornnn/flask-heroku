5097a627c022fffde18d59ecffe7e04a8a0990f5
thanapornnn <89174540+thanapornnn@users.noreply.github.com>
2021-09-02 12:20:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello ธนพร ธรรมมะ เลขที่17 ห้อง 4/5"
+    return "Hello ธนพร ธรรมมะ เลขที่17 ห้อง4/5"
 
 @app.route('/api', methods=['GET'])
 def get_api():
